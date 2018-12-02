742c25341e316f5ddc5864b774f7362eb9d6ee30
Liam <ldonohoe123@gmail.com>
2018-12-02 16:00:40 -0500

    Good hmac probably? Fixed sha_1 for bigger messages

--- hmac.py
+++ hmac.py
@@ -1,14 +1,21 @@
 #HMAC implementation
-import sha_1.py
 from __future__ import print_function
+from itertools import cycle, izip
+import sha_1
 import io
 import struct
+import string
+
+
+trans_5C = bytes((x ^ 0x5C) for x in range(256))
+trans_36 = bytes((x ^ 0x36) for x in range(256))
 
 
 
 def addPad(key, size):
 	while len(key) < size:
 		key += '0'
+	return key
 
 #Inputs - key - byte array
 #		- m - message to be hashed
@@ -27,10 +34,36 @@ def hmac(key, m):
 	if len(key) < block:
 		key = addPad(key,block)
 
-	o_key_pad = key ^ (0x5c * block)
-	i_key_pad = key ^ (0x36 * block)
+	tbl1 = string.maketrans(trans_5C, trans_5C)
+	tbl2 = string.maketrans(trans_36, trans_36)
 
-	inner = Hasha1().update(i_key_pad + m).hexDigest()
-	outer = Hasha1().update(o_key_pad + inner).hexDigest()
+	o_key_pad = key.translate(tbl1)
+	i_key_pad = key.translate(tbl2)
+
+
+	preIn = i_key_pad + m
+	print(preIn)
+	inner = sha_1.sha1(preIn)
+	print(inner)
+	preOut = o_key_pad + inner
+	outer = sha_1.sha1(preOut)
 
 	return outer
+
+
+
+def main():
+
+	m = "The quick brown fox jumps over the lazy dog"
+	k = b'key'
+
+
+
+	h = hmac(k,m)
+	
+	print(m)
+	print("HMAC :")
+	print(h)
+
+if __name__ == "__main__":
+	main()
