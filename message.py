ad8c0759480129789262b7a28739e602ea141865
Ehren <omegathesigma@gmail.com>
2018-12-03 17:29:19 -0500

    added a comment of removed hmac

--- message.py
+++ message.py
@@ -41,6 +41,7 @@ def encrypt(enc_type, message, key):
         message = des.encrypt(message, key)
     elif enc_type == 2:
         message = RC4.encrypt(message, key)
+	#message = message + hmac(key, message)
     return message
 def decrypt(dec_type, message, key):
     if dec_type == 0:
